27854a0b7870a2adc651650b2ce247af23e4c7bf
men <aaa@gggg.com>
2019-03-07 15:41:23 +0900

    Initial commit

---
+++ src/exam.java
@@ -0,0 +1,13 @@
+public class exam {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("Hello Spring 프로그래밍!!!");
+
+        while(true) {
+
+        }
+    }
+
+}
