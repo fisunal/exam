73543f930520e8f281527d7eba0694f79eb38c74
men <aaa@gggg.com>
2019-03-07 16:04:14 +0900

    nothing

---
+++ exam/src/exam/exam.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Spring Boot 프로그래밍!!!");
+		
+		while(true) {
+			
+		}
+	}
+
+}
